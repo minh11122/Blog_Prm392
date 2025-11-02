078cb66cc327a97b3e5d54081471ba83c16cc55e
PALALA\bthin <bthinh003@gmail.com>
2025-11-02 16:43:07 +0700

    FINISH: 16- RecyclerView

--- My_Food_App/app/src/main/java/com/example/myfoodapp/adapters/DailyMealAdapter.java
+++ My_Food_App/app/src/main/java/com/example/myfoodapp/adapters/DailyMealAdapter.java
@@ -1,4 +1,61 @@
 package com.example.myfoodapp.adapters;
 
-public class DailyMealAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myfoodapp.R;
+import com.example.myfoodapp.models.DailyMealModel;
+
+import java.util.List;
+
+public class DailyMealAdapter extends RecyclerView.Adapter<DailyMealAdapter.ViewHolder> {
+
+    Context context;
+    List<DailyMealModel> list;
+
+    public DailyMealAdapter(Context context, List<DailyMealModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new DailyMealAdapter.ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.daily_meal_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        DailyMealModel meal = list.get(position);
+
+        holder.imageView.setImageResource(meal.getImage());
+        holder.name.setText(meal.getName());
+        holder.discount.setText(meal.getDiscount());
+        holder.description.setText(meal.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView name, discount, description;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView1);
+            name = itemView.findViewById(R.id.textView9);
+            description = itemView.findViewById(R.id.textView10);
+            discount = itemView.findViewById(R.id.discount);
+        }
+    }
 }
--- My_Food_App/app/src/main/java/com/example/myfoodapp/ui/dailymeal/DailyMealFragment.java
+++ My_Food_App/app/src/main/java/com/example/myfoodapp/ui/dailymeal/DailyMealFragment.java
@@ -1,4 +1,4 @@
-package com.example.myfoodapp.ui.gallery;
+package com.example.myfoodapp.ui.dailymeal;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,23 +7,42 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myfoodapp.R;
+import com.example.myfoodapp.adapters.DailyMealAdapter;
+import com.example.myfoodapp.models.DailyMealModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DailyMealFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    List<DailyMealModel> dailyMealModels;
+    DailyMealAdapter dailyMealAdapter;
 
-
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
+        View root = inflater.inflate(R.layout.daily_meal_fragment, container, false);
 
+        recyclerView = root.findViewById(R.id.daily_meal_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        View root = inflater.inflate(R.layout.daily_meal_fragment, container, false) ;
+        dailyMealModels = new ArrayList<>();
+        dailyMealModels.add(new DailyMealModel(R.drawable.breakfast, "Breakfast","30% OFF", "Description Description"));
+        dailyMealModels.add(new DailyMealModel(R.drawable.fav3, "Lunch","20% OFF", "Description Description"));
+        dailyMealModels.add(new DailyMealModel(R.drawable.dinner, "Dinner","50% OFF", "Description Description"));
+        dailyMealModels.add(new DailyMealModel(R.drawable.icecream4, "Sweets","39% OFF", "Description Description"));
+        dailyMealModels.add(new DailyMealModel(R.drawable.coffe, "Coffe","20% OFF", "Description Description"));
 
+        dailyMealAdapter = new DailyMealAdapter(getContext(), dailyMealModels);
+        recyclerView.setAdapter(dailyMealAdapter);
+        dailyMealAdapter.notifyDataSetChanged();
 
         return root;
     }
-
-
 }
