58221375a351b7ac25acdb32964a166d64c5fa7e
PALALA\bthin <bthinh003@gmail.com>
2025-11-02 13:19:14 +0700

    FINISH: 11-RecyclerView

--- My_Food_App/app/src/main/java/com/example/myfoodapp/ui/home/HomeFragment.java
+++ My_Food_App/app/src/main/java/com/example/myfoodapp/ui/home/HomeFragment.java
@@ -14,8 +14,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myfoodapp.R;
 import com.example.myfoodapp.adapters.HomeHorAdapter;
+import com.example.myfoodapp.adapters.HomeVerAdapter;
 import com.example.myfoodapp.databinding.FragmentHomeBinding;
 import com.example.myfoodapp.models.HomeHorModel;
+import com.example.myfoodapp.models.HomeVerModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +25,16 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
 
-    RecyclerView homeHorizontalRec;
+    RecyclerView homeHorizontalRec, homeVerticalRec;
+
+    //////////////////////HorizontalRec//////////////////////
     List<HomeHorModel> homeHorModelList;
     HomeHorAdapter homeHorAdapter;
+
+
+    //////////////////////VerticalRec//////////////////////
+    List<HomeVerModel> homeVerModelList;
+    HomeVerAdapter homeVerAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,7 +42,9 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false) ;
 
         homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+        homeVerticalRec = root.findViewById(R.id.home_ver_rec);
 
+        //////////////////////Horizontal RecyclerView//////////////////////
         homeHorModelList = new ArrayList<>();
         homeHorModelList.add(new HomeHorModel(R.drawable.pizza,"Pizza"));
         homeHorModelList.add(new HomeHorModel(R.drawable.hamburger,"Hambuger"));
@@ -47,8 +58,18 @@ public class HomeFragment extends Fragment {
         homeHorizontalRec.setHasFixedSize(true);
         homeHorizontalRec.setNestedScrollingEnabled(false);
 
+        //////////////////////Vertical RecyclerView//////////////////////
+        homeVerModelList = new ArrayList<>();
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza1, "Pizaa", "10:00-23:00","4.9","Min-$34"));
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza2, "Pizaa", "10:00-23:00","4.9","Min-$35"));
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza3, "Pizaa", "10:00-23:00","4.9","Min-$36"));
+        homeVerModelList.add(new HomeVerModel(R.drawable.pizza4, "Pizaa", "10:00-23:00","4.9","Min-$37"));
 
-
+        homeVerAdapter = new HomeVerAdapter(getActivity(), homeVerModelList);
+        homeVerticalRec.setAdapter(homeVerAdapter);
+        homeVerticalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        homeVerticalRec.setHasFixedSize(true);
+        homeVerticalRec.setNestedScrollingEnabled(false);
         return root;
     }
 
