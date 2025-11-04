8eb403c63f38daafed86358393999c4e16bf5726
Tuấn Minh <136489572+minh11122@users.noreply.github.com>
2025-11-04 19:05:39 +0700

    FINISHED 19

--- My_Food_App/app/src/main/java/com/example/myfoodapp/activities/DetailedDailyMealActivity.java
+++ My_Food_App/app/src/main/java/com/example/myfoodapp/activities/DetailedDailyMealActivity.java
@@ -1,16 +1,30 @@
 package com.example.myfoodapp.activities;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myfoodapp.R;
+import com.example.myfoodapp.adapters.DetailedDailyAdapter;
+import com.example.myfoodapp.models.DetailedDailyModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailedDailyMealActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<DetailedDailyModel> detailedDailyModelList;
+    DetailedDailyAdapter dailyAdapter;
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +36,15 @@ public class DetailedDailyMealActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        recyclerView = findViewById(R.id.detailed_rec);
+        imageView = findViewById(R.id.detailed_img);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        detailedDailyModelList = new ArrayList<>();
+        dailyAdapter = new DetailedDailyAdapter(detailedDailyModelList);
+        recyclerView.setAdapter(dailyAdapter);
     }
 }
