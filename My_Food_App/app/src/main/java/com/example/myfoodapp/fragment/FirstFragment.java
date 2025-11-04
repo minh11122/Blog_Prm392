37a413ed0ff113264928a04ecd49ab8988c747b9
DNC <dinhcuonglc2003@gmail.com>
2025-11-05 04:26:55 +0700

    no message

--- My_Food_App/app/src/main/java/com/example/myfoodapp/fragment/FirstFragment.java
+++ My_Food_App/app/src/main/java/com/example/myfoodapp/fragment/FirstFragment.java
@@ -3,64 +3,53 @@ package com.example.myfoodapp.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myfoodapp.R;
+import com.example.myfoodapp.adapters.FeaturedAdapter;
+import com.example.myfoodapp.models.FeaturedModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FirstFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    List<FeaturedModel> featuredModelList;
+    RecyclerView recyclerView;
+    FeaturedAdapter featuredAdapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public FirstFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        recyclerView = view.findViewById(R.id.featured_hor_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.HORIZONTAL,false));
+        featuredModelList = new ArrayList<>();
+
+        featuredModelList.add(new FeaturedModel(R.drawable.fav1,"Featured 1","Description 1"));
+        featuredModelList.add(new FeaturedModel(R.drawable.fav3,"Featured 2","Description 2"));
+        featuredModelList.add(new FeaturedModel(R.drawable.fav2,"Featured 3","Description 3"));
+
+        featuredAdapter = new FeaturedAdapter(featuredModelList);
+        recyclerView.setAdapter(featuredAdapter);
+
+
+
+       return view;
     }
 }
